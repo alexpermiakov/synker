78b04457e09061579fb29712428df5edb23a774e
Alex Permiakov <alex.permyakoff@gmail.com>
2024-09-09 21:43:39 +0000

    add htons for when initialize client connection

--- src/client/network_operations/copy_file.c
+++ src/client/network_operations/copy_file.c
@@ -57,7 +57,7 @@ void copy_file (char *src_full_path, char *dst_full_path) {
 
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET; 
-  server_addr.sin_port = port;
+  server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = inet_addr(domain_name);
 
   connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
