c8de47db9c21780281178a7f29fe48bca0618058
Alex Permiakov <alex.permyakoff@gmail.com>
2024-09-19 18:08:19 +0000

    add handling for excess_bytes

--- src/server/client_handler.c
+++ src/server/client_handler.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #include "utils/file_utils.h"
 #include "client_handler.h"
@@ -36,19 +37,21 @@ int handle_client(connection_t *conn) {
       printf("File path: %s\n", conn->file_attrs.file_path);
       printf("File operation: %hhu\n", conn->file_attrs.operation);
 
+      size_t excess_bytes = conn->total_read - attr_size;
+
       if (conn->file_attrs.operation == CREATE_DIR) {
         if (mkdir(conn->file_attrs.file_path, conn->file_attrs.mode & 0777) < 0) {
           perror("mkdir");
           return -1;
         }
         conn->state = READING_FILE_ATTRS;
-        conn->total_read -= attr_size;
+        conn->total_read = 0;
         conn->expected_size = sizeof(file_attrs_t);
         conn->file_fd = -1;
       } else if (conn->file_attrs.operation == CREATE_FILE) {
         conn->state = READING_FILE_DATA;
         conn->expected_size = conn->file_attrs.size;
-        conn->total_read = 0;
+        conn->total_read = excess_bytes;
         conn->file_fd = open(conn->file_attrs.file_path, O_CREAT | O_WRONLY | O_TRUNC, conn->file_attrs.mode & 0777);
         
         printf("Updated state %d\n", conn->state);
@@ -59,8 +62,8 @@ int handle_client(connection_t *conn) {
           return -1;
         }
 
-        if (conn->total_read > attr_size) {
-          if (write(conn->file_fd, conn->buffer + attr_size, conn->total_read - attr_size) < 0) {
+        if (excess_bytes > 0) {
+          if (write(conn->file_fd, conn->buffer + attr_size, excess_bytes) < 0) {
             break;
           }
         } else {
@@ -70,7 +73,15 @@ int handle_client(connection_t *conn) {
         conn->state = DELETING_FILE;
         printf("Updated state %d\n", conn->state);
       }
-     
+
+      if (excess_bytes == 0) {
+        conn->total_read = 0;
+      }
+
+      if (excess_bytes > 0) {
+        memmove(conn->buffer, conn->buffer + attr_size, excess_bytes);
+        conn->total_read = excess_bytes;
+      }
     } if (conn->state == READING_FILE_DATA) {
       while (conn->total_read < conn->expected_size) {
         size_t to_read = BUFSIZ;
